52aa682256850412ed5ee6260fce1df9ad955721
vidhya-31 <vidhyashakthi09@gmail.com>
2026-06-18 11:29:17 +0530

    updated main.py

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from core.app_context import AppContext
 
 from database.database_manager import DatabaseManager
 
+
 from database.repositories.user_repository import UserRepository
 from database.repositories.instrument_repository import InstrumentRepository
 
 from controllers.login_controller import LoginController
 from controllers.app_controller import AppController
-
+# from controllers.historical_trend_controller import HistoricalTrendController
+from instruments.instrument_manager import InstrumentManager
 from views.home_window import HomeScreen
+from views.menu_window import MenuWindow
 
 
 def main():
@@ -59,19 +62,44 @@ def main():
         app
     )
 
+    # app.historical_trend_controller = HistoricalTrendController(app)
+    
+    app.instrument_manager = InstrumentManager(
+    app
+)
     # =====================================
     # HOME SCREEN
     # =====================================
 
-    app.home_screen = HomeScreen(
-        root,
-        app
+    # app.home_screen = HomeScreen(
+    #     root,
+    #     app
+    # )
+
+    # app.home_screen.pack(
+    #     fill="both",
+    #     expand=True
+    # )
+
+    app.home_screen = HomeScreen(root, app)
+
+    app.menu_window = MenuWindow(root, app)
+
+    app.home_screen.place(
+        relx=0,
+        rely=0,
+        relwidth=1,
+        relheight=1
     )
 
-    app.home_screen.pack(
-        fill="both",
-        expand=True
+    app.menu_window.place(
+        relx=0,
+        rely=0,
+        relwidth=1,
+        relheight=1
     )
+
+    app.home_screen.tkraise()
 
     root.mainloop()
 

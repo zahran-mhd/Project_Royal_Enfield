2f60f9ea5cc6ce2f7f03bb9886036caa8bbab080
krishnakumari-2002 <kala.ammu1979@gmail.com>
2026-07-21 16:51:38 +0530

    changes in the parameter page.

--- widgets/parameter_widget.py
+++ widgets/parameter_widget.py
@@ -139,7 +139,7 @@ def create_table(parent, columns, rows=3):
             table,
             text=text,
             bg="#3b5bdb",
-            fg="white",
+            fg="yellow",
             font=("Arial", 11, "bold"),
             relief="solid",
             bd=1,
@@ -174,9 +174,11 @@ def create_table(parent, columns, rows=3):
 
             frame = tk.Frame(
                 table,
-                bg="white",
+                bg="black",
                 relief="solid",
-                bd=1
+                bd=1,
+                width=80,
+                height=28
             )
 
             frame.grid(
@@ -185,19 +187,22 @@ def create_table(parent, columns, rows=3):
                 sticky="nsew"
             )
 
+            frame.grid_propagate(False)
+
+            value = tk.StringVar()
+
             entry = ttk.Entry(
                 frame,
-                justify="center"
+                justify="center",
+                textvariable=value
             )
 
             entry.pack(
                 fill="both",
                 expand=True,
-                padx=2,
-                pady=2
+                padx=1,
+                pady=1
             )
-
-    return table
 
 def create_bottom_button(parent, text, color):
 
@@ -213,3 +218,394 @@ def create_bottom_button(parent, text, color):
     )
 
     return btn
+
+
+def section_title(parent, text, row, column, columnspan=1):
+
+    label = tk.Label(
+        parent,
+        text=text,
+        bg="white",
+        font=("Arial",12,"bold")
+    )
+
+    label.grid(
+        row=row,
+        column=column,
+        columnspan=columnspan,
+        sticky="w",
+        pady=(15,15)
+    )
+
+    return label
+
+
+def table_header(parent, text, row, column):
+
+    lbl = tk.Label(
+        parent,
+        text=text,
+        bg="#f2f2f2",
+        relief="solid",
+        bd=1,
+        font=("Arial",10,"bold"),
+        padx=12,
+        pady=7
+    )
+
+    lbl.grid(
+        row=row,
+        column=column,
+        sticky="nsew"
+    )
+
+    return lbl
+
+
+def table_label(parent, text, row, column):
+
+    lbl = tk.Label(
+        parent,
+        text=text,
+        bg="white",
+        relief="solid",
+        bd=1,
+        padx=12,
+        width=14,  
+        pady=10
+    )
+
+    lbl.grid(
+        row=row,
+        column=column,
+        sticky="nsew"
+    )
+
+    return lbl
+
+def table_entry(parent, row, column):
+
+    frame = tk.Frame(
+        parent,
+        bg="black",      # Border color
+        relief="solid",
+        bd=1,
+        width=120,
+        height=38
+    )
+
+    frame.grid(
+        row=row,
+        column=column,
+        sticky="nsew"
+    )
+
+    frame.grid_propagate(False)
+
+    entry = ttk.Entry(
+        frame,
+        justify="center"
+    )
+
+    entry.pack(
+        fill="both",
+        expand=True,
+        padx=1,
+        pady=1
+    )
+
+    return entry
+
+
+def load_header(parent):
+
+    top = tk.Frame(parent, bg="white")
+    top.pack(fill="x", padx=40, pady=(15,25))
+
+    # Configure all columns
+    for i in range(9):
+        top.grid_columnconfigure(i, weight=0)
+
+    # Dwell Time
+    tk.Label(
+        top,
+        text="Dwell Time",
+        bg="white",
+        font=("Arial",11,"bold")
+    ).grid(row=0, column=0, sticky="w", padx=(0,8))
+
+    dwell_time = ttk.Entry(
+        top,
+        width=10
+    )
+
+    dwell_time.grid(
+        row=0,
+        column=1,
+        sticky="w"
+    )
+
+    tk.Label(
+        top,
+        text="Sec",
+        bg="white",
+        font=("Arial",11)
+   ).grid(row=0, column=2, sticky="w", padx=(5,70))
+
+
+    # Input Voltage
+    tk.Label(
+        top,
+        text="Input Voltage",
+        bg="white",
+        font=("Arial",11,"bold")
+    ).grid(row=0, column=3, sticky="w", padx=(0,8))
+
+    input_voltage = ttk.Entry(
+        top,
+        width=10
+    )
+
+    input_voltage.grid(
+        row=0,
+        column=4,
+        sticky="w"
+    )
+    tk.Label(
+        top,
+        text="V",
+        bg="white",
+        font=("Arial",11)
+   ).grid(row=0, column=5, sticky="w", padx=(5,70))
+
+
+    # Input Frequency
+    tk.Label(
+        top,
+        text="Input Frequency",
+        bg="white",
+        font=("Arial",11,"bold")
+    ).grid(row=0, column=6, sticky="w", padx=(0,8))
+
+    input_frequency = ttk.Entry(
+        top,
+        width=10
+    )
+
+    input_frequency.grid(
+        row=0,
+        column=7,
+        sticky="w"
+    )
+
+    tk.Label(
+        top,
+        text="Hz",
+        bg="white",
+        font=("Arial",11)
+    ).grid(row=0, column=8, sticky="w", padx=(5,0))
+
+    return {
+    "frame": top,
+    "dwell_time": dwell_time,
+    "input_voltage": input_voltage,
+    "input_frequency": input_frequency
+}
+
+
+
+def regulation_step(parent, title):
+
+    frame = tk.Frame(
+        parent,
+        bg="white"
+    )
+
+    # Column sizes
+    frame.grid_columnconfigure(0, minsize=90)
+    frame.grid_columnconfigure(1, minsize=70)
+    frame.grid_columnconfigure(2, minsize=95)
+    frame.grid_columnconfigure(3, minsize=95)
+
+    # ==========================
+    # Header Row
+    # ==========================
+
+    tk.Label(
+        frame,
+        bg="white",
+        relief="solid",
+        bd=1
+    ).grid(row=0, column=0, sticky="nsew")
+
+    tk.Label(
+        frame,
+        bg="white",
+        relief="solid",
+        bd=1
+    ).grid(row=0, column=1, sticky="nsew")
+
+    tk.Label(
+        frame,
+        text="HV Load (%)",
+        bg="yellow",
+        font=("Arial",10,"bold"),
+        relief="solid",
+        bd=1
+    ).grid(row=0, column=2, sticky="nsew")
+
+    tk.Label(
+        frame,
+        text="Set HV\nCurrent (A)",
+        bg="#fff176",
+        font=("Arial",10,"bold"),
+        relief="solid",
+        bd=1
+    ).grid(row=0, column=3, sticky="nsew")
+
+    # ==========================
+    # Left merged cell
+    # ==========================
+
+    tk.Label(
+        frame,
+        text=title,
+        bg="yellow",
+        font=("Arial",12),
+        justify="center",
+        relief="solid",
+        bd=1
+    ).grid(
+        row=1,
+        column=0,
+        rowspan=5,
+        sticky="nsew"
+    )
+
+    # ==========================
+    # Middle merged Entry
+    # ==========================
+
+    entry_frame = tk.Frame(
+        frame,
+        bg="yellow",
+        relief="solid",
+        bd=1
+    )
+
+    entry_frame.grid(
+        row=1,
+        column=1,
+        rowspan=5,
+        sticky="nsew"
+    )
+
+    step_voltage = ttk.Entry(
+    entry_frame,
+    justify="center"
+    )
+
+    step_voltage.pack(
+        fill="both",
+        expand=True,
+        padx=1,
+        pady=1
+    )
+
+    # ==========================
+    # Right side rows
+    # ==========================
+
+    loads = [
+        "No Load",
+        "25",
+        "50",
+        "75",
+        "100"
+    ]
+    entries = {}
+
+    for i, value in enumerate(loads):
+
+        tk.Label(
+            frame,
+            text=value,
+            bg="yellow",
+            relief="solid",
+            bd=1
+        ).grid(
+            row=i+1,
+            column=2,
+            sticky="nsew"
+        )
+
+        eframe = tk.Frame(
+            frame,
+            bg="yellow",
+            relief="solid",
+            bd=1
+        )
+
+        eframe.grid(
+            row=i+1,
+            column=3,
+            sticky="nsew"
+        )
+
+        entry = ttk.Entry(
+            eframe,
+            justify="center"
+        )
+
+        entry.pack(
+            fill="both",
+            expand=True,
+            padx=1,
+            pady=1
+        )
+
+        entries[value] = entry
+
+    # RETURN AFTER THE LOOP
+    return {
+        "frame": frame,
+        "step_voltage": step_voltage,
+        "load_entries": entries
+        
+    }
+
+
+def load_regulation_panel(parent, title):
+
+    panel = tk.Frame(parent, bg="white")
+
+    tk.Label(
+        panel,
+        text=title,
+        bg="white",
+        font=("Arial",13,"bold")
+    ).pack(
+        anchor="center",
+        pady=(0,18)
+    )
+
+
+    steps = {}
+
+    for i in range(1,4):
+
+        step = regulation_step(
+            panel,
+            f"Step {i}\nHV\nVoltage"
+        )
+
+        step["frame"].pack(
+            pady=12
+        )
+
+        steps[f"step{i}"] = step
+
+    return {
+        "panel": panel,
+        "steps": steps
+    }

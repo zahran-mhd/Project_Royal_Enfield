428c42396af5c8f927228d9ef1c5a2dc9c3cef4e
zahran-mhd <zahran.mohammed@elmackengg.com>
2026-07-25 14:25:30 +0530

    added can files

---
+++ instruments/drivers/can_driver.py
@@ -0,0 +1,36 @@
+import can
+import cantools
+
+
+class CANDriver:
+
+    def __init__(self, channel, bitrate, dbc_file=None):
+
+        self.bus = can.interface.Bus(
+            interface="pcan",
+            channel=channel,
+            bitrate=bitrate
+        )
+
+        self.db = None
+
+        if dbc_file:
+            self.db = cantools.database.load_file(dbc_file)
+
+    def receive(self, timeout=0.01):
+
+        return self.bus.recv(timeout)
+
+    def send(self, arbitration_id, data, extended=False):
+
+        msg = can.Message(
+            arbitration_id=arbitration_id,
+            data=data,
+            is_extended_id=extended
+        )
+
+        self.bus.send(msg)
+
+    def shutdown(self):
+
+        self.bus.shutdown()
